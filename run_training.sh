5b3386636e60f3293fd76a8a13359fcd8d06a806
jcyinnovations <jcyinnovations@outlook.com>
2025-04-26 17:01:07 -0400

    Add control for discount factor to allow setting to 0.999

--- run_training.sh
+++ run_training.sh
@@ -2,6 +2,7 @@
 clear
 ITERATION=$1
 PARALLEL_ENVS=$2
+DISCOUNT_FACTOR=$3
 echo $VIRTUAL_ENV
-python rl_lstm_trader.py --timesteps=20000000 --discount_factor=0.99 --eval_frequency=750 --checkpoint_frequency=50 --iteration="$ITERATION" --parallel_envs="$PARALLEL_ENVS" > training_"$ITERATION".log
+python rl_lstm_trader.py --timesteps=20000000 --discount_factor="$DISCOUNT_FACTOR" --eval_frequency=750 --checkpoint_frequency=50 --iteration="$ITERATION" --parallel_envs="$PARALLEL_ENVS" > training_"$ITERATION".log
 
